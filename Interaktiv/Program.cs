232c2858960a891937fa0627b173914e0de42688
Aris Paschalidis <aris.paschalidis@elev.ga.ntig.se>
2024-09-03 12:52:19 +0200

    liugliug

---
+++ Interaktiv/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+
+
+
+
+// Console.WriteLine("Är du lång?");
+// string tall = Console.ReadLine();
+
+// if (tall == "ja" || tall == "JA" || tall == "jA" || tall == "jA")
+// {
+//     Console.WriteLine("Kul för dig?");
+// }
+// else if (tall == "kanske")
+// {
+//     Console.WriteLine("okej?");
+// }
+//     else
+// {
+//     Console.WriteLine("trist...");
+// }
+// Console.ReadLine();
